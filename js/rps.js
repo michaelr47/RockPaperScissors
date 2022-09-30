8099d71cc2b8e4f23e100b2989c88adbb61e5a1e
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-09-29 20:33:03 -0700

    feat title, user & comp score, bg color, score container

---
+++ js/rps.js
@@ -0,0 +1,17 @@
+let userScore = document.getElementById('user');
+let compScore = document.getElementById('comp');
+
+
+const game = (type) => {
+    
+    if (type === 'rock') {
+        let userS = parseInt(userScore.innerHTML);
+        userS++;
+    } else if (type === 'paper') {
+        
+    } else if (type === 'scissors') {
+
+    }
+   
+}
+
